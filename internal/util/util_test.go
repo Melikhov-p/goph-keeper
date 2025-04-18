0eb1d0ef8cccb2ba354e480aa9e015075e26f17e
Pavel <72454035+Melikhov-p@users.noreply.github.com>
2025-04-18 17:30:26 +0300

    add test for utils

---
+++ internal/util/util_test.go
@@ -0,0 +1,72 @@
+package util
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSafeConvertToInt32(t *testing.T) {
+	convertable := 156
+	unConvertable := math.MaxUint32 + 1
+	minUnConvertable := math.MinInt32 - 1
+
+	testCases := []struct {
+		name     string
+		value    int
+		wantErr  bool
+		expected int32
+	}{
+		{
+			name:     "success",
+			value:    convertable,
+			wantErr:  false,
+			expected: int32(convertable),
+		},
+		{
+			name:     "tooBig",
+			value:    unConvertable,
+			wantErr:  true,
+			expected: *new(int32),
+		},
+		{
+			name:     "tooShort",
+			value:    minUnConvertable,
+			wantErr:  true,
+			expected: *new(int32),
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			res, err := SafeConvertToInt32(test.value)
+
+			if !test.wantErr {
+				assert.NoError(t, err)
+				assert.IsType(t, *new(int32), res)
+			} else {
+				assert.Error(t, err)
+			}
+
+			assert.Equal(t, test.expected, res)
+		})
+	}
+}
+
+func ExampleSafeConvertToInt32() {
+	valueInt := 1
+	valueInt32, err := SafeConvertToInt32(valueInt)
+	fmt.Println("1: ", valueInt32, err)
+
+	bigValueInt := math.MaxUint32 + 1
+	bigValueInt32, bigErr := SafeConvertToInt32(bigValueInt)
+	fmt.Println("2: ", bigValueInt32, bigErr)
+
+	// Output:
+
+	// 1: 1 nil
+
+	// 2: 0, value is out of range int32
+}
